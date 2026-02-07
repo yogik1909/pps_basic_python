1154f95ba3a2a6f2ff8391c1475bd5a6ddac3b60
Vova <white_net@mail.ru>
2026-02-07 22:09:40 +1000

    figured out IDs under hood

---
+++ 6. oop intro/id_simple_object.py
@@ -0,0 +1,13 @@
+a = 10
+print(f"a: {a}")
+print(f"id(a): {id(a)}")
+b = a
+print(f"b: {b}")
+print(f"id(b): {id(b)}")
+print("Выполнили операцию над переменной а, и снова выедем их значения и идентификаторы")
+a += 10
+
+print(f"a: {a}")
+print(f"id(a): {id(a)}")
+print(b)
+print(f"id(b): {id(b)}")
