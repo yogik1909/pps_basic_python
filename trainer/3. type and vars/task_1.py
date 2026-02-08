c49f5984cc1d1e016ea533a8f64fc48a107d0e53
Vova <white_net@mail.ru>
2026-02-08 23:49:32 +1000

    goal 1

---
+++ trainer/3. type and vars/task_1.py
@@ -0,0 +1,24 @@
+# **Описание**: Создайте переменные для хранения информации о пользователе и выведите их типы
+#
+# **Входные данные**: Нет (используйте встроенные значения в коде)
+#
+# **Выходные данные**: Типы переменных, выведенные с помощью функции type()
+#
+# **Ограничения**: Используйте только базовые типы данных Python: int, float, str, bool
+#
+# **Примеры**:
+# Вывод должен содержать типы в формате:
+# <class 'int'>
+# <class 'float'>
+# <class 'str'>
+# <class 'bool'>
+
+person_name = "John"
+person_age = 30
+person_height = 1.80
+person_is_student = True
+
+print(type(person_name))
+print(type(person_age))
+print(type(person_height))
+print(type(person_is_student))
