8910272b6028c9e1ec765d2b77b0c97f552b43f6
Vova <white_net@mail.ru>
2026-02-10 21:23:12 +1000

    Refactor rock-paper-scissors game logic: improved user input validation and streamlined win condition checks.

--- 8. Cycles/rock-paper_scissors.py
+++ 8. Cycles/rock-paper_scissors.py
@@ -4,39 +4,34 @@
 # У кого больше побед тот и победил
 import random
 
-a = ["ROC", "PAPER", "SCISSORS"]
-# Компьютер выбирает случайное число из списка
-computer_choice = random.choice(a)
+CHOISES = ["ROCK", "PAPER", "SCISSORS"]
 # Пользователь вводит число раудов
 num_round = int(input("Enter the number of rounds: "))
 sum_res_use = 0
 sum_res_comp = 0
+user_choice = ""
 for i in range(num_round):
-    user_choice = input("Enter your choice: ")
-    computer_choice = random.choice(a)
-    
+    while user_choice not in CHOISES:
+        user_choice = input("Enter your choice: ")
+
+    computer_choice = random.choice(CHOISES)
+
     match user_choice:
-        case "ROC":
-            if computer_choice == "SCISSORS":
-                sum_res_use += 1
-            else:
-                sum_res_comp += 1
-        case "PAPER":
-            if computer_choice == "ROC":
-                sum_res_use += 1
-            else:
-                sum_res_comp += 1
-        case "SCISSORS":
-            if computer_choice == "PAPER":
-                sum_res_use += 1
-            else:
-                sum_res_comp += 1
+        case "ROCK" if computer_choice == "SCISSORS":
+            sum_res_use += 1
+        case "PAPER" if computer_choice == "ROCK":
+            sum_res_use += 1
+        case "SCISSORS" if computer_choice == "PAPER":
+            sum_res_use += 1
         case _:
-            print("Invalid choice")
+            sum_res_comp += 1
+    user_choice = ""
 
 print(f"You win {sum_res_use} times")
 print(f"Computer win {sum_res_comp} times")
 if sum_res_use > sum_res_comp:
     print("You win")
+elif sum_res_use == sum_res_comp:
+    print("Draw")
 else:
     print("Computer win")
